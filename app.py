6b6b924081cc5ca57feeddb23283a2ca116ec53a
Nadia Bourial <nadia.bourial@gmail.com>
2024-03-05 12:08:57 +0000

    Adding login and sign up buttons and pages

--- app.py
+++ app.py
@@ -15,6 +15,14 @@ app = Flask(__name__)
 def get_index():
     return render_template('index.html')
 
+@app.route('/login', methods=['GET'])
+def get_login():
+    return render_template('login.html')
+
+@app.route('/sign_up', methods=['GET'])
+def get_sign_up():
+    return render_template('sign_up.html')
+
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
 # if started in test mode.
