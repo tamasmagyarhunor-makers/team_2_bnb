7e70762ad8e80af46e713dbbf14426d401d3c241
Lloyd <lloyd585@outlook.com>
2024-03-05 13:45:32 +0000

    self.price and owner id updated to match sql seed

--- lib/space.py
+++ lib/space.py
@@ -1,15 +1,15 @@
 class Space:
-    def __init__(self, id, name, location, description, price_per_night, owner_id):
+    def __init__(self, id, name, location, description, price, user_id):
         self.id = id
         self.name = name
         self.location = location
         self.description = description
-        self.price_per_night = price_per_night
-        self.owner_id = owner_id
+        self.price = price
+        self.user_id = user_id
 
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
     
     def __repr__(self):
-        return f"{self.id}, {self.name}, {self.location}, {self.description}, {self.price_per_night}, {self.owner_id}"
+        return f"{self.id}, {self.name}, {self.location}, {self.description}, {self.price}, {self.user_id}"
