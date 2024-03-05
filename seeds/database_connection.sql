e257c51458ba600a1369c38042551db31b720f90
Nadia Bourial <nadia.bourial@gmail.com>
2024-03-05 12:37:45 +0000

    Commit fix to table space_availability

--- seeds/database_connection.sql
+++ seeds/database_connection.sql
@@ -4,6 +4,20 @@
 -- database state, and that tests don't interfere with each other.
 
 -- First, we must delete (drop) all our tables
+DROP TABLE IF EXISTS test_table;
+
+-- Then, we recreate them
+CREATE TABLE test_table (id SERIAL PRIMARY KEY, name VARCHAR(255));
+
+-- Finally, we add any records that are needed for the tests to run
+INSERT INTO test_table (name) VALUES ('first_record');
+
+-- The job of this file is to reset all of our important database tables.
+-- And add any data that is needed for the tests to run.
+-- This is so that our tests, and application, are always operating from a fresh
+-- database state, and that tests don't interfere with each other.
+
+-- First, we must delete (drop) all our tables
 DROP TABLE IF EXISTS users cascade;
 DROP TABLE IF EXISTS spaces cascade;
 DROP TABLE IF EXISTS requests cascade;
@@ -43,6 +57,7 @@ CREATE TABLE requests (
 
 CREATE TABLE space_availability (
   id SERIAL PRIMARY KEY,
+  space_id, int
   date_1 boolean,
   date_2 boolean,
   date_3 boolean,
